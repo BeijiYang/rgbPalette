1fbf888701ac036c6e797ca0bbc8fef7bf2ac92b
BeijiYang <langyuyang@outlook.com>
2020-09-07 18:35:24 +0200

    reactive v1

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const callbacks = [];
+// object 作为 key，以便找相应的 reactivities
+const callbacks = new Map();
+let usedReactivities = [];
 
 // 不可 observe
 const object = {
@@ -14,7 +16,20 @@ effect(() => {
 })
 
 function effect(cb) {
-  callbacks.push(cb);
+  usedReactivities = [];
+  cb();
+  console.log(usedReactivities) // [   [po, "a"]   ]
+  for (const reactivity of usedReactivities) {
+    // eactivity[0] 即作为 key 的 object
+    if (!callbacks.has(reactivity[0])) {
+      callbacks.set(reactivity[0], new Map());
+    }
+    // 对象的属性 prop
+    if (!callbacks.get(reactivity[0]).has(reactivity[1])) {
+      callbacks.get(reactivity[0]).set(reactivity[1], []);
+    }
+    callbacks.get(reactivity[0]).get(reactivity[1]).push(cb);
+  }
 }
 
 function reactive(object) {
@@ -22,13 +37,17 @@ function reactive(object) {
     set(obj, prop, val) {
       obj[prop] = val;
 
-      for (const cb of callbacks) {
-        cb();
+      if (callbacks.get(obj) && callbacks.get(obj).get(prop)) {
+        for (const cb of callbacks.get(obj).get(prop)) {
+          cb();
+        }
       }
+      // console.log(callbacks)
       return obj[prop];
     },
     get(obj, prop) {
-      console.log(obj, prop);
+      // 注册
+      usedReactivities.push([obj, prop]);
       return obj[prop];
     }
   })
