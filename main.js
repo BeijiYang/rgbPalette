bcab81f6bd2417a76e8da1e35833951482fd5e2f
BeijiYang <langyuyang@outlook.com>
2020-09-07 18:02:07 +0200

    reactive v0

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
+const callbacks = [];
+
 // 不可 observe
 const object = {
   a: 1,
   b: 2,
 };
 // 可以 observe
-const po = new Proxy(object, {
-  set(obj, prop, val) {
-    console.log(obj, prop, val);
-  }
+const po = reactive(object);
+
+// 监听 po 上的属性，代替事件监听的机制
+effect(() => {
+  console.log(po.a);
 })
+
+function effect(cb) {
+  callbacks.push(cb);
+}
+
+function reactive(object) {
+  return new Proxy(object, {
+    set(obj, prop, val) {
+      obj[prop] = val;
+
+      for (const cb of callbacks) {
+        cb();
+      }
+      return obj[prop];
+    },
+    get(obj, prop) {
+      console.log(obj, prop);
+      return obj[prop];
+    }
+  })
+}
