bd1bbbd437bc6b9ce432a7554eb99bc26b432e33
BeijiYang <langyuyang@outlook.com>
2020-09-07 18:51:42 +0200

    to support po.a.b

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 // object 作为 key，以便找相应的 reactivities
 const callbacks = new Map();
 let usedReactivities = [];
+// 全局的缓存，防止在 po.a.b 时，由于新包装的一层，导致 effect 中，与 reactive 中访问到的不是同一个 proxy
+const reactives = new Map();
 
 // 不可 observe
 const object = {
-  a: 1,
+  a: { b: 3 },
   b: 2,
 };
 // 可以 observe
@@ -12,7 +14,7 @@ const po = reactive(object);
 
 // 监听 po 上的属性，代替事件监听的机制
 effect(() => {
-  console.log(po.a);
+  console.log(po.a.b);
 })
 
 function effect(cb) {
@@ -33,7 +35,9 @@ function effect(cb) {
 }
 
 function reactive(object) {
-  return new Proxy(object, {
+  if (reactives.has(object)) return reactives.get(object);
+
+  const proxy = new Proxy(object, {
     set(obj, prop, val) {
       obj[prop] = val;
 
@@ -48,7 +52,13 @@ function reactive(object) {
     get(obj, prop) {
       // 注册
       usedReactivities.push([obj, prop]);
+      // 当 obj[prop] 是对象，套一层 reactive，以支持 po.a.b 的情况
+      if (typeof obj[prop] === 'object') {
+        return reactive(object[prop]);
+      }
       return obj[prop];
     }
   })
+  reactives.set(object, proxy);
+  return proxy;
 }
